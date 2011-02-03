9f88927762f26b1444a01e9cf1da1f2c45a22750
makheshwar <makheshwar@gmail.com>
2011-02-02 22:22:38 -0800

    provide an option to specify the ui version

--- lib/generators/jquery/install/install_generator.rb
+++ lib/generators/jquery/install/install_generator.rb
@@ -4,7 +4,9 @@ module Jquery
       desc "This generator downloads and installs jQuery, jQuery-ujs HEAD, and (optionally) the newest jQuery UI"
       class_option :ui, :type => :boolean, :default => false, :desc => "Include jQueryUI"
       class_option :version, :type => :string, :default => "1.4.4", :desc => "Which version of jQuery to fetch"
+      class_option :ui_version, :type => :string, :default =>"1.8.9", :desc => "Which version of jQuery ui to fetch"
       @@default_version = "1.4.4"
+      @@default_ui_version = "1.8.9"
 
       def remove_prototype
         %w(controls.js dragdrop.js effects.js prototype.js).each do |js|
@@ -23,9 +25,13 @@ module Jquery
 
       def download_jquery_ui
         if options.ui?
-          say_status("fetching", "jQuery UI (latest 1.x release)", :green)
-          get "http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.js",     "public/javascripts/jquery-ui.js"
-          get "http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js", "public/javascripts/jquery-ui.min.js"
+          say_status("fetching", "jQuery ui (#{options.ui_version})", :green)
+          get_jquery_ui(options.ui_version)
+          rescue OpenURI::HTTPError
+             say_status("warning", "could not find jQuery (#{options.ui_version})", :yellow)
+             say_status("fetching", "jQuery (#{@@default_version})", :green)
+             get_jquery_ui(@@default_ui_version)
+          end          
         end
       end
 
@@ -40,6 +46,11 @@ module Jquery
         get "http://ajax.googleapis.com/ajax/libs/jquery/#{version}/jquery.js",     "public/javascripts/jquery.js"
         get "http://ajax.googleapis.com/ajax/libs/jquery/#{version}/jquery.min.js", "public/javascripts/jquery.min.js"
       end
+
+     def get_jquery_ui(version)
+          get "http://ajax.googleapis.com/ajax/libs/jqueryui/#{version}/jquery-ui.js",     "public/javascripts/jquery-ui.js"
+          get "http://ajax.googleapis.com/ajax/libs/jqueryui/#{version}/jquery-ui.min.js", "public/javascripts/jquery-ui.min.js"
+     end
 
     end
   end
